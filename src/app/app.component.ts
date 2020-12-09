b0f02871f4d3ca94e5e8cbd0402a401a95db76ac
Nicole Bartholomew <nabartholomew@gmail.com>
2020-12-08 18:09:25 -0600

    (feat) Added same page navigation scroll.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { ViewportScroller } from '@angular/common';
+import { Target } from '@angular/compiler';
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
@@ -10,7 +12,8 @@ export class AppComponent {
   @HostListener ('window:scroll', ['$event'])
     onWindowScroll(event) {
       let nameTitleRect: ClientRect = this.nameTitle.nativeElement.getBoundingClientRect();
-
+      
+      console.log(this.isNavClicked);
       if(nameTitleRect.bottom <= 0) {
         this.isHeaderSticky = true;
       }
@@ -20,5 +23,14 @@ export class AppComponent {
     }
   title = 'portfolio-site';
   isHeaderSticky: boolean = false;
-  
+  isNavClicked: boolean = false;
+
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  onPageNavigate(elementId: Element): void {
+    // this.isHeaderSticky = true;
+    // this.viewportScroller.scrollToAnchor(elementId);
+    elementId.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    
+  }
 }
